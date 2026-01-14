53ab85c028706dc9c515067e0f7744ecf30cd6d4
Bhavnoorgoyal <bbhavnoor_be24@thapar.edu>
2026-01-14 10:52:18 +0530

    Create q1.cpp

---
+++ assignment 1/q1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int binarySearch(int arr[], int n, int key) {
+    int low=0, high=n-1;
+    while (low<= high) {
+        int mid = low+(high - low) / 2;
+        if (arr[mid]==key)
+            return mid;
+        else if (arr[mid]<key)
+            low = mid+1;
+        else
+            high = mid-1;
+    }
+    return -1;
+}
+
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 23;
+    int result = binarySearch(arr,n,key);
+    if (result != -1)
+        cout << "element found at index" << result;
+    else
+        cout << "element not found";
+    return 0;
+}
+
+
